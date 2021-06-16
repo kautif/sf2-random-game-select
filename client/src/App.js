bc4bd83d0b9dd3e3fabd18a7a7f5900850834dee
Autif Kamal <kautif@gmail.com>
2021-06-15 20:07:28 -0400

    redirect to endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GameIconContainer from './components/GameIconContainer';
 import MusicComponent from './components/MusicComponent';
-import { BrowserRouter as Router, Route, Link, BrowserRouter, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, BrowserRouter, Switch, Redirect } from "react-router-dom";
 import CoverComponent from './components/CoverComponent';
 
 function App() {
+  let isAuth = false;
   return (
     <div className="App">
+      {isAuth ? 
+      window.location.href = "http://localhost:3001/api" : 
       <Router>
         <Route exact path="/" component={CoverComponent}></Route>
         <Route path="/random_select" component={GameIconContainer}></Route>  
-      </Router>
+      </Router>}
       {/* <MusicComponent /> */}
       {/* <GameIconContainer /> */}
     </div>
